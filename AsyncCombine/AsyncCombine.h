2f555fcb962b14adee63645707f654db74aa34e7
tosh7 <zlia.6.lj.425@gmail.com>
2022-12-14 00:06:46 +0900

    Initial Commit

---
+++ AsyncCombine/AsyncCombine.h
@@ -0,0 +1,18 @@
+//
+//  AsyncCombine.h
+//  AsyncCombine
+//
+//  Created by Satoshi Komatsu on 2022/12/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncCombine.
+FOUNDATION_EXPORT double AsyncCombineVersionNumber;
+
+//! Project version string for AsyncCombine.
+FOUNDATION_EXPORT const unsigned char AsyncCombineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncCombine/PublicHeader.h>
+
+
