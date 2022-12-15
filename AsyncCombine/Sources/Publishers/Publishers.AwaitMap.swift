34b3a930eae63c428b9763831cd946c30e92471e
tosh7 <zlia.6.lj.425@gmail.com>
2022-12-15 23:58:14 +0900

    Modified AwaitMap

--- AsyncCombine/Sources/Publishers/Publishers.AwaitMap.swift
+++ AsyncCombine/Sources/Publishers/Publishers.AwaitMap.swift
@@ -9,5 +9,16 @@ import Foundation
 import Combine
 
 extension Publisher {
-    
+    public func awaitMap<Result>(
+        _ transform: @escaping (Output) -> Result
+    ) ->  Publishers.Map<Self, Result> {
+        return Publishers.AwaitMap(upstream: self, transform: transform)
+    }
+}
+
+extension Publisher {
+    public struct AwaitMap<Upstream: Publisher, Output>: Publisher {
+
+
+    }
 }
