8bb1deba875eb6d25aa24594e2cf92d58d26f981
tosh7 <zlia.6.lj.425@gmail.com>
2023-05-10 23:40:49 +0900

    Create example

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Satoshi Komatsu on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
